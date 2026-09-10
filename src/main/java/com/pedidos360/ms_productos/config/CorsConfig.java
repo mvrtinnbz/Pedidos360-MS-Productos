cdc2a71115c91779f7ae9fe95cecd0e651ea9d11
mvrtinnbz <martinbazas2004@gmail.com>
2026-09-10 02:04:51 -0300

    Version: Fix: Cors

--- src/main/java/com/pedidos360/ms_productos/config/CorsConfig.java
+++ src/main/java/com/pedidos360/ms_productos/config/CorsConfig.java
@@ -12,8 +12,7 @@ public class CorsConfig {
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:4200",
-            "https://TU-DOMINIO-FRONTEND.nip.io"
+            "*"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
